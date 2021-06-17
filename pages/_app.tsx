fed72957cee1148d84efe636b71723d77595d619
NgocThang-MJ <tranngocthang.bkdn@gmail.com>
2021-06-17 16:31:29 +0700

    change favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 // import "../dist/styles.css";
 import "tailwindcss/tailwind.css";
 import "../src/styles.css";
@@ -9,6 +10,9 @@ import LayoutPage from "../components/layout/Layout";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LayoutPage>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </LayoutPage>
   );
