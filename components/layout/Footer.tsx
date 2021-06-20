961b3454065314371e7edbfe45f7c5a243849999
NgocThang-MJ <tranngocthang.bkdn@gmail.com>
2021-06-20 16:54:58 +0700

    Fix text align footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
   return (
     <div className="bg-gradient-to-t from-top via-middle to-bottom text-white">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mx-auto w-11/12 pb-6 pt-56">
-        <div className="text-center md:text-right">
+        <div className="text-center md:text-left">
           <p className="text-4xl font-dancing mb-4">Liquid Manga</p>
           <p className="my-2 text-sm">
             Read latest, updated manga at Liquid Manga
